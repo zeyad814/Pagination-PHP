afa537d026278ae07d3771b807c158b319031842
zeyad <zeyadmohamed1423@gmail.com>
2024-09-26 11:16:50 +0300

    card title description update

--- show.php
+++ show.php
@@ -29,9 +29,9 @@ $products = $query->fetchAll();
             <img src="..." class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
-                    card's
-                    content.</p>
+                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos tempora, ut voluptate
+                    quod culpa a nisi labore tenetur modi, magnam ipsa ducimus et aliquam vero? Atque sint minima
+                    distinctio corrupti!</p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
