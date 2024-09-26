08a066451aa4eb14b64158dff4a53b4a13439469
zeyad <zeyadmohamed1423@gmail.com>
2024-09-26 11:24:13 +0300

    delete description

--- show.php
+++ show.php
@@ -29,9 +29,7 @@ $products = $query->fetchAll();
             <img src="..." class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
-                <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos tempora, ut voluptate
-                    quod culpa a nisi labore tenetur modi, magnam ipsa ducimus et aliquam vero? Atque sint minima
-                    distinctio corrupti!</p>
+                <p class="card-text"></p>
                 <a href="#" class="btn btn-primary">Go somewhere</a>
             </div>
         </div>
